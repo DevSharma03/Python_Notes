0c7ba082b946a8af8ed349824657c8d7945ddb51
Devashish Sharma <168680367+DevSharma03@users.noreply.github.com>
2025-06-14 13:49:04 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a Number 1:")) # 5
+print(a);
+c = int(input("Enter a Number 2:")) # 10
+print(c);
+b = type(a);
+print(b);
+d = a + c;
+print(d);  # the result will be 15 as tge inputs are integer 
